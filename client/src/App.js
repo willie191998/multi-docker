720bbf00b4f4e7f414fc6046b6b8de0ccaf555a9
Wilson Anorue <willie191998@gmail.com>
2024-04-18 11:50:51 +0100

    3rd trial

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Please, Learn React ...
+            Please, Learn React Now ...
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
